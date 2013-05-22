0152f63e08a693d14c7c57aff8985a7f1ffe505b
Ian Martin <ian@imartin.net>
2013-05-21 23:29:10 -0600

    Added stuff to models

--- invoice/models.py
+++ invoice/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Invoice(models.Model):
+	def _get_grand_total(self):
+		return 120.02
+
+	grand_total = property(_get_grand_total)
--- lineitem/models.py
+++ lineitem/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class LineItem(models.Model):
+	invoice = models.ForeignKey('invoice.Invoice')
+	description = models.CharField(max_length=120)
+	quantity = models.IntegerField()
+	taxable = models.BooleanField()
+	unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+	def _get_total(self):
+		return self.quantity * self.unit_price
+
+	total = property(_get_total)
+	
