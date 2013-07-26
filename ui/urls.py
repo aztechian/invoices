2930e84846d8bdd9a3dd655f460b20d31814c037
Ian Martin <imartin@solidyn.com>
2013-07-26 15:30:30 -0400

    WIP on Customer page, client JS structures

--- ui/urls.py
+++ ui/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
-from ui.views import LandingPageView
+from ui.views import LandingPageView, InvoiceDetailView, CustomerView
 
 urlpatterns = patterns('',
-	url(r'^$', LandingPageView.as_view())
+	url(r'^$', LandingPageView.as_view()),
+	url(r'^invoices/', InvoiceDetailView.as_view()),
+	url(r'^customers/', CustomerView.as_view())
 )
