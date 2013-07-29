5f204ef4c6a05e435f9d4320a6dbca3352b4a166
Ian Martin <imartin@solidyn.com>
2013-07-29 03:44:45 -0400

    Much more working on customer page. Can submit new customers.

--- ui/static/js/customer.js
+++ ui/static/js/customer.js
@@ -8,20 +8,39 @@ var customer = (function($) {
 		var self = this,
 		tempPk;
 		
-		self.first_name = ko.observable(serverData.first_name);
-		self.last_name = ko.observable(serverData.last_name);
-		self.email = ko.observable(serverData.email);
-		self.is_active = ko.observable(serverData.is_active);
-		self.date_joined = ko.observable(serverData.date_joined);
-		self.street1 = ko.observable(serverData.street1);
-		self.street2 = ko.observable(serverData.street2);
-		self.city = ko.observable(serverData.city);
-		self.state = ko.observable(serverData.state);
-		self.zip_code = ko.observable(serverData.zip_code);
-		self.phone = ko.observable(serverData.phone);
-		tempPk = parseInt(serverData.url.split('/').slice(-2,-1)[0], 10);
-		self.pk = ko.observable(tempPk);
-		self.username = ko.observable(serverData.username);
+		if( serverData === undefined || serverData === null ){
+			self.first_name = ko.observable("");
+			self.last_name = ko.observable("");
+			self.email = ko.observable("");
+			self.is_active = ko.observable(true);
+			self.date_joined = ko.observable("");
+			self.street1 = ko.observable("");
+			self.street2 = ko.observable("");
+			self.city = ko.observable("");
+			self.state = ko.observable("");
+			self.zip_code = ko.observable("");
+			self.phone = ko.observable("");
+			self.pk = ko.observable(0);
+			self.username = ko.observable("");
+			self.password = ko.observable("");
+		}
+		else {
+			self.first_name = ko.observable(serverData.first_name);
+			self.last_name = ko.observable(serverData.last_name);
+			self.email = ko.observable(serverData.email);
+			self.is_active = ko.observable(serverData.is_active);
+			self.date_joined = ko.observable(serverData.date_joined);
+			self.street1 = ko.observable(serverData.street1);
+			self.street2 = ko.observable(serverData.street2);
+			self.city = ko.observable(serverData.city);
+			self.state = ko.observable(serverData.state);
+			self.zip_code = ko.observable(serverData.zip_code);
+			self.phone = ko.observable(serverData.phone);
+			tempPk = parseInt(serverData.url.split('/').slice(-2,-1)[0], 10);
+			self.pk = ko.observable(tempPk);
+			self.username = ko.observable(serverData.username);
+			self.password = ko.observable(serverData.password);
+		}
 		
 		self.formatted_phone = ko.computed(function(){
 			var orig = String(self.phone());
@@ -35,15 +54,21 @@ var customer = (function($) {
 			if( self.street2().trim().length > 0 )
 				tmpAddr += " " + self.street2();
 			tmpAddr += " " + self.city() + ", " + self.state() + " " + self.zip_code();
+			if( tmpAddr.trim() === ',')
+				tmpAddr = '';
 			return (tmpAddr.length > 30) ? tmpAddr.slice(0,30) + "..." : tmpAddr;
 		});
 		
 		self.full_name = ko.computed(function(){
 			return self.first_name() + " " + self.last_name();
 		});
-	};
+	},
 	
-	var init = function(){
+	addCustomer = function(){
+		customerList.push(new CustomerViewModel());
+	},
+	
+	init = function(){
 		$.getJSON('/api/customers/', function(data){
 			$.each(data, function(i, val){
 				var cust = new CustomerViewModel(val);
@@ -57,7 +82,9 @@ var customer = (function($) {
 	
 	return {
 		init: init,
+		addCustomer: addCustomer,
 		customerList: customerList
+		
 	};
 }(jQuery));
 
